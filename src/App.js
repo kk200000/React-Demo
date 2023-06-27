20500aad5f10a3651e515578f22953f4b18747b0
Your Name <826697618@qq.com>
2023-06-27 10:58:19 +0800

    init demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Button,Alert,Navbar,Container,InputGroup,FormControl, Card,CardChecklist } from 'react-bootstrap';
+import { useState } from 'react';
+
+function Todo() {
+  return [
+    {name:'abc',isCheck:false,id:1},
+    {name:'ppp',isCheck:true,id:2},
+    {name:'kyl',isCheck:false,id:3}
+  ]
+}
+
+function Item(props) {
+  const [todo,setTodo] = useState(Todo()) 
+  return( <InputGroup >
+    <InputGroup.Checkbox checked={props.isCheck} onChange={props.onToggle}></InputGroup.Checkbox>
+        <FormControl  value={props.name} style={{textDecoration:props.isCheck?"line-through 4px":"none"}}></FormControl>
+        <Button onClick={props.onDelete} >
+          
+          {/* <Trash></Trash> */}
+          删除
+        </Button>
+    </InputGroup>)
+  
+  
+ 
+}
+
 function App() {
+  const [todo,setTodo] = useState(Todo()) 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+       <Container>
+          <Navbar.Brand>
+             待办事项
+          </Navbar.Brand>
+       </Container>
+         
+      </Navbar>
+
+
+      <Container>
+      {
+          todo.map(item=>(
+            <Container>
+             <Item 
+             id = {item.id}
+             isCheck = {item.isCheck}
+             name = {item.name}
+             onToggle = {()=>{
+              setTodo(todo.map((x)=>x.name === item.name ? {...x,isCheck:!x.isCheck}:x))
+             }}
+             onDelete={
+          ()=>{
+          setTodo(todo.filter((x) => x.name !== item.name)) 
+        }}></Item>
+           
+        </Container>
+          ))
+        }
+      </Container>
+ 
+    </>
+    
+    // <div className="App">
+    //   {
+    //     todo.map((item)=>(
+    //       <>
+    //       <input type='checkbox' defaultChecked={todo.isCheck}></input>
+    //       <label>{item.name}</label>
+    //       </>
+    //     ))
+    //   }
+
+
+
+
+
+    // </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// 引入boot
+import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // 严格模式
+  // 影响useEffect 执行时机
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
